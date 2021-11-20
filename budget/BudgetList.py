34f4e85f70308ec6289408e391c46ca5edc1dd6c
Owen Utley <90651143+owenutley@users.noreply.github.com>
2021-11-19 19:26:49 -0700

    finished budget list with error

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -49,7 +49,10 @@ def main():
 
     fig, ax = plt.subplots()
     labels = ["Expenses", "Overages", "Budget"]
-    values = myBudgetList
+    values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
+    ax.bar(labels, values, color=['green', 'red', 'blue'])
+    ax.set_title('Your total expenses vs. total budget')
+    plt.show()
 
 if __name__ == "__main__":
     main()
